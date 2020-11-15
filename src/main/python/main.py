a96cb86eab025dc6fbbbf8c9c4b795a390fbbea3
livkx <15910716+livkx@users.noreply.github.com>
2020-11-15 21:35:31 +0000

    Added settings option to tray icon, now pops up a settings menu with some settings. Just need to add some functionality to the settings and later make the menu a lot less ugly, but im terrible with GUI stuff

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,16 +1,16 @@
 import sys
-
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QSystemTrayIcon, QMenu, QAction, QWidget, QWidgetAction, QSlider
-from PyQt5.QtCore import Qt
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from shutil import copyfile
 import contextlib
 import os
+
+from shutil import copyfile
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtWidgets import QSystemTrayIcon, QMenu, QAction, QWidget, QWidgetAction, QSlider, QMainWindow, QDialog, QFormLayout, QLabel, QCheckBox, QComboBox, QDialogButtonBox
+from PyQt5.QtCore import Qt, QSettings, QRect, QMetaObject, QCoreApplication
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+
 import pulsectl
 
 pulse = pulsectl.Pulse("t")
-
 
 class CadmusPulseInterface:
     @staticmethod
@@ -81,7 +81,7 @@ class CadmusApplication(QSystemTrayIcon):
 
         self.disable_suppression_menu = QAction("Disable Noise Suppression")
         self.enable_suppression_menu = QMenu("Enable Noise Suppression")
-        self.settings_menu = QMenu("Settings")
+        self.settings_menu = QAction("Settings")
         self.level_section = None
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setTickInterval(5)
@@ -125,14 +125,11 @@ class CadmusApplication(QSystemTrayIcon):
             self.enable_suppression_menu.addAction(mic_menu_item)
             mic_menu_item.triggered.connect(self.enable_noise_suppression)
         self.exit_menu.triggered.connect(self.quit)
-
-    #Add the settings options into the menu here
-
-        #self.settings_menu.addAction("temp")
+        self.settings_menu.triggered.connect(self.open_settings)
 
         main_menu.addMenu(self.enable_suppression_menu)
         main_menu.addAction(self.disable_suppression_menu)
-        main_menu.addMenu(self.settings_menu)
+        main_menu.addAction(self.settings_menu)
         main_menu.addAction(self.exit_menu)
 
         # Add slider widget
@@ -144,28 +141,85 @@ class CadmusApplication(QSystemTrayIcon):
         self.setIcon(self.disabled_icon)
         self.setContextMenu(main_menu)
 
-    def disable_noise_suppression(self):
-        CadmusPulseInterface.unload_modules()
-        self.disable_suppression_menu.setEnabled(False)
-        self.enable_suppression_menu.setEnabled(True)
-        self.setIcon(self.disabled_icon)
-
     def enable_noise_suppression(self):
         CadmusPulseInterface.load_modules(self.sender().mic_name, self.cadmus_lib_path)
         self.setIcon(self.enabled_icon)
         self.enable_suppression_menu.setEnabled(False)
         self.disable_suppression_menu.setEnabled(True)
 
+    def disable_noise_suppression(self):
+        CadmusPulseInterface.unload_modules()
+        self.disable_suppression_menu.setEnabled(False)
+        self.enable_suppression_menu.setEnabled(True)
+        self.setIcon(self.disabled_icon)
+    
+    def open_settings(self):
+        settingswindow.show()
+
     def quit(self):
         self.disable_noise_suppression()
         self.app_context.app.quit()
 
+class CadmusSettings(QMainWindow):
+    def __init__(self,app_context,parent:None):
+        QMainWindow.__init__(self, parent)
+        self.setWindowTitle("Cadmus Settings")
+        self.app_context = app_context
+        self.setObjectName("CadmusSettings")
+        self.resize(688, 199)
+        self.widget = QWidget()
+        self.widget.setGeometry(QRect(190, 20, 308, 157))
+        self.widget.setObjectName("widget")
+        self.setCentralWidget(self.widget)
+        self.formLayout = QFormLayout(self.widget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QLabel(self.widget)
+        font = QFont()
+        font.setPointSize(18)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+        self.check_LoadLogon = QCheckBox(self.widget)
+        font = QFont()
+        font.setPointSize(12)
+        self.check_LoadLogon.setFont(font)
+        self.check_LoadLogon.setObjectName("check_LoadLogon")
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.check_LoadLogon)
+        self.check_EnableOnStartup = QCheckBox(self.widget)
+        font = QFont()
+        font.setPointSize(12)
+        self.check_EnableOnStartup.setFont(font)
+        self.check_EnableOnStartup.setObjectName("check_EnableOnStartup")
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.check_EnableOnStartup)
+        self.combo_DefMic = QComboBox(self.widget)
+        self.combo_DefMic.setObjectName("combo_DefMic")
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.combo_DefMic)
+        self.buttonBox_SettingsDialog = QDialogButtonBox(self.widget)
+        self.buttonBox_SettingsDialog.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox_SettingsDialog.setObjectName("buttonBox_SettingsDialog")
+        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.buttonBox_SettingsDialog)
+
+        self.retranslateUi()
+        QMetaObject.connectSlotsByName(parent)
+
+    def retranslateUi(self):
+        _translate = QCoreApplication.translate
+        self.setWindowTitle("Cadmus Settings")
+        self.label.setText(_translate("CadmusSettings", "Cadmus Settings"))
+        self.check_LoadLogon.setText(_translate("CadmusSettings", "Load Cadmus on logon?"))
+        self.check_EnableOnStartup.setText(_translate("CadmusSettings", "Enable noise supression on startup?"))
+        self.combo_DefMic.setCurrentText(_translate("CadmusSettings", "Default Microphone for noise supression on startup"))
+        self.combo_DefMic.setPlaceholderText(_translate("CadmusSettings", "Default Microphone for noise supression on startup"))
 
 if __name__ == "__main__":
     cadmus_context = ApplicationContext()
+    cadmus_context.app.setQuitOnLastWindowClosed(False)
     parent_widget = QWidget()
 
     icon = CadmusApplication(cadmus_context, parent_widget)
     icon.show()
+
+    settingswindow = CadmusSettings(cadmus_context, parent_widget)
 
     sys.exit(cadmus_context.app.exec_())
